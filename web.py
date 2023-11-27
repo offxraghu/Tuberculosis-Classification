23edc65eccdb5a96d46426c7adcdd84fc05d079b
G Raghavendra <147441167+offxraghu@users.noreply.github.com>
2023-11-27 15:05:06 +0530

    Update web.py

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ def loading_model():
 cnn = loading_model()
 st.write("""
 # Cloud Based Web Application Tuberculosis Detection Using CNN App
-Built by Jayes and Team
+Built by G Raghavendra and Team
 """)
 
 
@@ -35,7 +35,7 @@ if buffer:
 
 
 if buffer is None:
-    st.text("This model is built for public use by Jayeskumar M, Jeevanatham V, Kruthickraj R, Marimuthu S")
+    st.text("This model is built for public use by G Raghavendra")
 
 else:
 
